ef6aced6e52178abd32fdfbeafd2da0dc9213f10
i2tsuki <mac-daisuki@live.jp>
2019-03-10 06:30:57 +0900

    Handle write error

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -69,83 +69,68 @@ pub fn exec(app: clap::App) -> Result<(), CmdError> {
     buf_reader.read_to_string(&mut cue_sheet)?;
 
     let cd = CD::parse(cue_sheet)?;
-    buf_writer
-        .write(
-            format!(
-                "PERFORMER \"{}\"\n",
-                cd.get_cdtext().read(PTI::Performer).ok_or(
-                    "performer is not found"
-                        .to_string(),
-                )?
-            ).as_bytes(),
-        )
-        .unwrap();
-    buf_writer
-        .write(
-            format!(
-                "TITLE \"{}\"\n",
-                cd.get_cdtext().read(PTI::Title).ok_or(
-                    "title is not found"
-                        .to_string(),
-                )?
-            ).as_bytes(),
-        )
-        .unwrap();
-
-    buf_writer
-        .write(
-            format!(
-                "REM DATE \"{}\"\n",
-                cd.get_rem().read(RemType::Date as usize).ok_or(
-                    "date is not found"
-                        .to_string(),
-                )?
-            ).as_bytes(),
-        )
-        .unwrap();
-    buf_writer
-        .write(
-            format!(
-                "GENRE \"{}\"\n",
-                cd.get_cdtext().read(PTI::Genre).ok_or(
-                    "genre is not found"
-                        .to_string(),
-                )?
-            ).as_bytes(),
-        )
-        .unwrap();
-    buf_writer
-        .write(
-            format!("FILE \"{}\" WAVE\n", cd.tracks()[0].get_filename()).as_bytes(),
-        )
-        .unwrap();
+    buf_writer.write(
+        format!(
+            "PERFORMER \"{}\"\n",
+            cd.get_cdtext().read(PTI::Performer).ok_or(
+                "performer is not found".to_string(),
+            )?
+        ).as_bytes(),
+    )?;
+    buf_writer.write(
+        format!(
+            "TITLE \"{}\"\n",
+            cd.get_cdtext().read(PTI::Title).ok_or(
+                "title is not found"
+                    .to_string(),
+            )?
+        ).as_bytes(),
+    )?;
+    buf_writer.write(
+        format!(
+            "REM DATE \"{}\"\n",
+            cd.get_rem().read(RemType::Date as usize).ok_or(
+                "date is not found".to_string(),
+            )?
+        ).as_bytes(),
+    )?;
+    buf_writer.write(
+        format!(
+            "GENRE \"{}\"\n",
+            cd.get_cdtext().read(PTI::Genre).ok_or(
+                "genre is not found"
+                    .to_string(),
+            )?
+        ).as_bytes(),
+    )?;
+    buf_writer.write(
+        format!(
+            "FILE \"{}\" WAVE\n",
+            cd.tracks()[0].get_filename()
+        ).as_bytes(),
+    )?;
 
     for (index, track) in cd.tracks().iter().enumerate() {
-        buf_writer
-            .write(format!("  TRACK {:>02} AUDIO\n", index + 1).as_bytes())
-            .unwrap();
-        buf_writer
-            .write(
-                format!(
-                    "    TITLE \"{}\"\n",
-                    track.get_cdtext().read(PTI::Title).ok_or(
-                        "track title is not found"
-                            .to_string(),
-                    )?
-                ).as_bytes(),
-            )
-            .unwrap();
-        buf_writer
-            .write(
-                format!(
-                    "    PERFORMER \"{}\"\n",
-                    track.get_cdtext().read(PTI::Performer).ok_or(
-                        "track performer is not found"
-                            .to_string(),
-                    )?
-                ).as_bytes(),
-            )
-            .unwrap();
+        buf_writer.write(
+            format!("  TRACK {:>02} AUDIO\n", index + 1)
+                .as_bytes(),
+        )?;
+        buf_writer.write(
+            format!(
+                "    TITLE \"{}\"\n",
+                track.get_cdtext().read(PTI::Title).ok_or(
+                    "track title is not found".to_string(),
+                )?
+            ).as_bytes(),
+        )?;
+        buf_writer.write(
+            format!(
+                "    PERFORMER \"{}\"\n",
+                track.get_cdtext().read(PTI::Performer).ok_or(
+                    "track performer is not found".to_string(),
+                )?
+            ).as_bytes(),
+        )?;
 
         // ToDo: Use `time_frame_to_msf(long frame, int *m, int *s, int *f)` to convert frame to msf
         // Ref:https://github.com/lipnitsk/libcue/blob/cbbde79f64042bef87f5c8b7661845525a04c97e/time.c#L26
@@ -158,27 +143,24 @@ pub fn exec(app: clap::App) -> Result<(), CmdError> {
         let index01_frame = (track.get_start() as u32 + track.get_index(1) as u32) % 75;
 
         if index != 0 {
-            buf_writer
-                .write(
-                    format!(
+            buf_writer.write(
+                format!(
                 "    INDEX 00 {0:>02}:{1:>02}:{2:>02}\n",
                 index00_min,
                 index00_sec,
                 index00_frame,
             ).as_bytes(),
-                )
-                .unwrap();
+            )?;
         }
-        buf_writer
-            .write(
-                format!(
+        buf_writer.write(
+            format!(
             "    INDEX 01 {0:>02}:{1:>02}:{2:>02}\n",
             index01_min,
             index01_sec,
             index01_frame,
         ).as_bytes(),
-            )
-            .unwrap();
+        )?;
+
     }
     buf_writer.flush()?;
 
